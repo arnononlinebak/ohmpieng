b1dfccaab9336f26c8581387bb7b9ce0dbbda6f7
arnononline <arnononline@me.com>
2014-08-27 01:55:50 +0700

    asdf

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'applications');
+define('DB_NAME', 'NonMadden');
 
 /** MySQL database username */
 define('DB_USER', 'non');
