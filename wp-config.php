441eaa32120a5598ceb09b6bf03e64cac828c6a1
arnononline <arnononline@me.com>
2014-08-30 22:29:38 +0700

    last update

--- wp-config.php
+++ wp-config.php
@@ -19,7 +19,7 @@
 define('DB_NAME', 'NonMadden');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'non');
 
 /** MySQL database password */
 define('DB_PASSWORD', ':vp.sh]n,F]d');
